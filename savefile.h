255c9148e6ca42e00372e40d8dcfc0a95f7dd08f
Joshua Ferrara <scjosh2@gmail.com>
2017-03-18 11:13:06 -0700

    Save file implementation

---
+++ savefile.h
@@ -0,0 +1,18 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+
+void savenote()  {
+	string title;
+	cout << "Enter a title for the note: ";
+	getline(cin, title);
+
+	string reminder;
+	cout << "Enter note for " << title << endl;
+	getline(cin, reminder);
+
+	ofstream out(title.append(".txt").c_str());
+	out << reminder;
+}
