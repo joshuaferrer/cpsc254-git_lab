a4f1453cae8986eb9f845d7be466e1c827afb0f5
George Calderon <gcald@csu.fullerton.edu>
2017-03-25 08:43:49 -0700

    Update areaSurfaceCube.h

--- areaSurfaceCube.h
+++ areaSurfaceCube.h
@@ -1,4 +1,13 @@
 #pragma once
-int areaSurfaceCube(int edge) {
-	return  6 * edge;
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+void areaSurfaceCube() {
+	int edge = 0;
+	cout << "Enter the size of the cube's edge." << endl;
+	cin >> edge;
+	cout << "The surface area of your cube is " << 6 * edge << "." << endl;
+	system("pause");
 }
