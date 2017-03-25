21d18a287f13239e6917c0fa51092fcd264f1764
Don Vu <don.vu1212@gmail.com>
2017-03-25 09:31:19 -0700

    Update maxOfNum.h

--- maxOfNum.h
+++ maxOfNum.h
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <algorithm>
 #include <fstream>
+#include <string>
 using namespace std;
 
-void maxList(){
+void maxList() {
 	int maximum = 0;
 	int comparison;
+	string filePath;
 
-	ifstream myfile("numberedList.txt");
+	cout << "Please input the file path name: ";
+	cin >> filePath;
+
+	ifstream myfile(filePath);
 
 	if (myfile.is_open())
-		while (myfile >> comparison){
-		maximum = (maximum, comparison);
+		while (myfile >> comparison) {
+			maximum = max(maximum, comparison);
 		}
-	}
 
 	cout << "The maximum number is: " << maximum << endl;
 }
-
